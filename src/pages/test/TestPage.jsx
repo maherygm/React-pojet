5c8d6ee281b4bbd7dcd8ceee90a61b261481b798
MAHERY <MAHERY@gmail.com>
2023-11-24 18:30:02 +0300

    slider card components fini

--- src/pages/test/TestPage.jsx
+++ src/pages/test/TestPage.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import ToDo from "../../globalComponents/toDoList/component/ToDo";
 import Vague from "../../globalComponents/customComponents/backgroundLandingPage/Vague/Vague";
 import Card_service from "../../globalComponents/customComponents/customCard/sampleCardHoverdeAnnimation/Card_service";
+import SliderCard from "../../globalComponents/slider/SliderCard";
 const TestPage = () => {
   return (
     <div>
-      {/* <ToDo /> */}
+      <ToDo />
       {/* <Vague /> */}
 
-      <Card_service
+      {/* <Card_service
         imgTexteProps={"web"}
         texteProps={"im am sample texte"}
         linkImgProps={""}
         titleProps={"Cards"}
-      />
+      /> */}
 
+      {/* <SliderCard /> */}
     </div>
   );
 };
