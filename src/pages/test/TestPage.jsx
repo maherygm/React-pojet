bd2f1d044f4bc09cf7521d4bcbfbcb5f3ff4db02
MAHERY <MAHERY@gmail.com>
2023-11-24 17:50:30 +0300

    component created

--- src/pages/test/TestPage.jsx
+++ src/pages/test/TestPage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import ToDo from "../../globalComponents/toDoList/component/ToDo";
+import Vague from "../../globalComponents/customComponents/backgroundLandingPage/Vague/Vague";
+import Card_service from "../../globalComponents/customComponents/customCard/sampleCardHoverdeAnnimation/Card_service";
 const TestPage = () => {
   return (
     <div>
-      <h1>Test Pages</h1>
+      {/* <ToDo /> */}
+      {/* <Vague /> */}
+
+      <Card_service
+        imgTexteProps={"web"}
+        texteProps={"im am sample texte"}
+        linkImgProps={""}
+        titleProps={"Cards"}
+      />
+
     </div>
   );
 };
