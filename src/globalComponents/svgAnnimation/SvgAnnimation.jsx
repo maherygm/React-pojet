c519e1f14ac8ca9612b357e685345db5fb150edf
MAHERY <MAHERY@gmail.com>
2023-12-04 11:22:49 +0300

    annimation svg

--- src/globalComponents/svgAnnimation/SvgAnnimation.jsx
+++ src/globalComponents/svgAnnimation/SvgAnnimation.jsx
@@ -10,50 +10,8 @@ const SvgAnnimation = () => {
     const context = gsap.context(() => {
       gsap.to(elementRef.current, {
         motionPath: {
-          path: [
-            {
-              x: 10,
-              y: 20,
-            },
-            {
-              x: 100,
-              y: 0,
-            },
-            {
-              x: 200,
-              y: 100,
-            },
-            {
-              x: 200,
-              y: 150,
-            },
-            {
-              x: 300,
-              y: 200,
-            },
-          ],
-          align: [
-            {
-              x: 10,
-              y: 20,
-            },
-            {
-              x: 100,
-              y: 0,
-            },
-            {
-              x: 200,
-              y: 100,
-            },
-            {
-              x: 200,
-              y: 150,
-            },
-            {
-              x: 300,
-              y: 200,
-            },
-          ],
+          path: "path",
+          align: "path",
           alignOrigin: [0.5, 0.5],
           autoRotate: true,
         },
@@ -63,7 +21,29 @@ const SvgAnnimation = () => {
       });
     }, elementRef);
   });
-  return <div className="svg" ref={elementRef}></div>;
+  return (
+    <>
+      <div className="svg" ref={elementRef}></div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="995.565"
+          height="455.516"
+          viewBox="0 0 995.565 455.516"
+        >
+          <path
+            id="Tracé_1"
+            data-name="Tracé 1"
+            d="M0,0S238.244-69.626,394.466,19.7c28.745,16.435,52.446,38.035,72.834,62.764,89.712,108.813,111.836,276.245,205.816,311C921.866,485.464,995,368,995,368"
+            transform="translate(0.14 26.356)"
+            fill="none"
+            stroke="#707070"
+            stroke-width="1"
+          />
+        </svg>
+      </div>
+    </>
+  );
 };
 
 export default SvgAnnimation;
